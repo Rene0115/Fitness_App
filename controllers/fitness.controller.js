762f0757d7c0b3ba4b3bfa781b500da461cedda1
Rene0115 <enere0115@gmail.com>
2023-04-21 15:52:22 +0100

    create fitness program

--- controllers/fitness.controller.js
+++ controllers/fitness.controller.js
@@ -1,10 +1,30 @@
 import fitnessModel from "../models/fitness.model.js";
-
+import { StatusCodes } from "http-status-codes";
 
 class FitnessController {
-    async create(req, res) {
-        const data = {
-            
-        }
+  async create(req, res) {
+    const data = {
+      ProgramName: req.body.ProgramName
+    };
+    if (!data) {
+      return res.status(StatusCodes.BAD_REQUEST).send({
+        success: false,
+        message: "Cannot create fitness program without name"
+      });
     }
+    const fitnessProgram = await fitnessModel.create(data);
+    if (!fitnessProgram) {
+        return res.status(StatusCodes.EXPECTATION_FAILED).send({
+            success: false,
+            message: "Something went wrong"
+        })
+    }
+
+    return res.status(StatusCodes.CREATED).send({
+        success: true,
+        data: fitnessProgram
+    })
+  }
+
+  
 }
