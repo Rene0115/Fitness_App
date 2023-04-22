a8fae3da6fa5b30ba7864eb1ee69879e2b4cc377
Rene0115 <enere0115@gmail.com>
2023-04-22 18:38:05 +0100

    create and delete fitness program complete

--- controllers/fitness.controller.js
+++ controllers/fitness.controller.js
@@ -1,6 +1,7 @@
 import fitnessModel from "../models/fitness.model.js";
 import { StatusCodes } from "http-status-codes";
-
+import _ from "lodash";
+import logger from "../app.js";
 class FitnessController {
   async create(req, res) {
     const data = {
@@ -14,17 +15,64 @@ class FitnessController {
     }
     const fitnessProgram = await fitnessModel.create(data);
     if (!fitnessProgram) {
-        return res.status(StatusCodes.EXPECTATION_FAILED).send({
-            success: false,
-            message: "Something went wrong"
-        })
+      return res.status(StatusCodes.EXPECTATION_FAILED).send({
+        success: false,
+        message: "Something went wrong"
+      });
     }
 
     return res.status(StatusCodes.CREATED).send({
-        success: true,
-        data: fitnessProgram
+      success: true,
+      data: fitnessProgram
+    });
+  }
+
+  async edit(req, res) {
+    const oldProgramName = req.body.programName;
+    const fitnessProgram = await fitnessModel.findOne({
+      ProgramName: oldProgramName
+    });
+
+    if (_.isEmpty(fitnessProgram)) {
+      return res.status(StatusCodes.NOT_FOUND).send({
+        success: false,
+        message: "Fitness program not found"
+      });
+    }
+    const newName = req.body.newName;
+
+    try {
+      fitnessProgram.updateOne({ ProgramName: newName });
+      await fitnessProgram.save();
+    } catch (err) {
+      logger.error(err);
+    }
+
+    return res.status(StatusCodes.OK).send({
+      success: true,
+      message: `Your fitness program name was changed from ${oldProgramName} to ${newName}`
+    });
+  }
+
+  async delete(req, res) {
+    try {
+      await fitnessModel.findByIdAndDelete(req.body.id);
+    } catch (err) {
+      logger.error(err);
+    }
+    return res.status(StatusCodes.ACCEPTED).send({
+      success: true,
+      message: `Fitness program successfully deleted`
     })
   }
 
+
+async createExercise(req, res) {
   
 }
+
+
+
+}
+
+export default new FitnessController();
--- routes/fitness.routes.js
+++ routes/fitness.routes.js
@@ -1,5 +1,8 @@
 import express from 'express';
+import fitnessController from '../controllers/fitness.controller.js';
 
 const fitnessRouter = express.Router();
+
+fitnessRouter.post('/create', fitnessController.create)
 
 export default fitnessRouter;
